0f7f014ed23571a42afe0af6dc1333bd00eb1f64
TP27 <101163840+celine-teh@users.noreply.github.com>
2022-04-13 15:46:38 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,6 @@
 <?php
 
-//Begin Really Simple SSL session cookie settings
+//Begin Really Simple SSL session cookie settings //test
 @ini_set('session.cookie_httponly', true);
 @ini_set('session.cookie_secure', true);
 @ini_set('session.use_only_cookies', true);
